d3b1edacd6ffad7698a4bd76061bcb5570f2179e
alee chou <zq@alee2002.com>
2014-04-26 22:12:35 +0800

    release 3dcode

--- Interface/AddOns/!!!163UI.3dcodecmd!!!/main.lua
+++ Interface/AddOns/!!!163UI.3dcodecmd!!!/main.lua
@@ -1,6 +1,5 @@
 local base_char,keywords=192,{"and","break","do","else","elseif","end","false","for","function","if","in","local","nil","not","or","repeat","return","then","true","until","while",}; function prettify(code) return code:gsub("["..string.char(base_char).."-"..string.char(base_char+#keywords).."]", 
   function (c) return keywords[c:byte()-base_char]; end) end return assert(loadstring(prettify[===[Ì É e(...)
-print("[verbose]",...)
 Æ
 Ì É t(...)
 print(...)
