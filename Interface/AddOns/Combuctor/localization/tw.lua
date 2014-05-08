4e3ac3f11aeb077b1a58bcae09b671829e832f98
zhaofeng <420617527@qq.com>
2014-05-08 20:19:38 +0800

    combuctor

---
+++ Interface/AddOns/Combuctor/localization/tw.lua
@@ -0,0 +1,41 @@
+--[[
+	Traditional Chinese
+		By Irene Wang
+--]]
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Combuctor", "zhTW")
+if not L then return end
+
+
+L.Updated = '更新到%s版'
+
+--binding actions
+L.ToggleInventory = "打開或關閉背包"
+L.ToggleBank = "打開或關閉銀行"
+
+--frame titles
+L.InventoryTitle = "%s的背包"
+L.BankTitle = "%s的銀行"
+
+--tooltips
+L.Inventory = '背包'
+L.Bank = '銀行'
+L.Total = '總'
+L.ClickToPurchase = '<點選>購買'
+L.Bags = '容器'
+L.BagToggle = '<左鍵點選>顯示或隱藏容器'
+L.InventoryToggle = '<右鍵點選>打開或關閉背包視窗'
+L.BankToggle = '<右鍵點選>打開或關閉銀行視窗'
+
+L.Normal = '一般'
+L.Equipment = '裝備'
+L.Trade = '商業'
+L.Ammo = '彈藥'
+L.Shards = '碎片'
+L.SoulShard = '靈魂碎片'
+L.Usable = '消耗品'
+
+--itemcount tooltips
+L.TipCount1 = ', 已裝備: %d'
+L.TipCount2 = ', 背包: %d'
+L.TipCount3 = ', 銀行: %d'
---
+++ Interface/AddOns/JPack/localization/localization.xx.lua
@@ -0,0 +1,8 @@
+--[=[
+	Language Localization: xxXX
+	Translated by: name <email address>
+]=]
+
+if GetLocale() ~= "xxXX" then return end
+local L = JPackLocale
+
