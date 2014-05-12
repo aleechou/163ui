d167b419b7599a425eb243975554de94a6e6d5d6
ubuntu <ubuntu@i-rsha8meu.gd1.qingcloud.com>
2014-05-12 21:54:34 +0800

    build new 3dcodecmd addon

--- Interface/AddOns/!!!163UI.3dcodecmd!!!/buffhacking.lua
+++ Interface/AddOns/!!!163UI.3dcodecmd!!!/buffhacking.lua
@@ -1,27 +1,46 @@
 local base_char,keywords=192,{"and","break","do","else","elseif","end","false","for","function","if","in","local","nil","not","or","repeat","return","then","true","until","while",}; function prettify(code) return code:gsub("["..string.char(base_char).."-"..string.char(base_char+#keywords).."]", 
-  function (c) return keywords[c:byte()-base_char]; end) end return assert(loadstring(prettify[===[Ì i="!!!163UI.3dcodecmd!!!"
+  function (c) return keywords[c:byte()-base_char]; end) end return assert(loadstring(prettify[===[Ì n="!!!163UI.3dcodecmd!!!"
 Ê bit==Í Á bit32~=Í Ò
 bitfunc=bit32
 Ä
 bitfunc=bit
 Æ
-Ì o=0
-Ì a=Ç
-Ì s=0
-Ì n=Ç
+Ì e={
+verbose=Ç,
+info=Ç,
+errro=Ó,
+}
+Ì É i(...)
+Ê e.verbose Ò print("[verbose]",...)Æ
+Æ
+Ì É t(...)
+Ê e.info Ò print("[verbose]",...)Æ
+Æ
+Ì É t(...)
+Ê e.errro Ò print("[verbose]",...)Æ
+Æ
+É cmd3dcode_buff_showlog(t,a)
+Ê t==Í Ò t="verbose"Æ
+Ê a==Í Ò a=Ó Æ
+e[t]=a
+Æ
+Ì a=0
+Ì o=Ç
+Ì h=0
+Ì s=Ç
 Ì e=CreateFrame("button","buff163uiLinkage_online",BuffFrame)
 e:SetWidth(BUFF_BUTTON_HEIGHT)
 e:SetHeight(BUFF_BUTTON_HEIGHT)
 Ì t=e:CreateTexture(Í,"BACKGROUND")
-t:SetTexture("Interface\\AddOns\\"..i.."\\Textures\\logo-online.tga")
+t:SetTexture("Interface\\AddOns\\"..n.."\\Textures\\logo-online.tga")
 t:SetAllPoints(e)
 e:SetScript("OnEnter",É(i)
 Ì e=""
 Ì t=0
-Ê o<60 Ò
+Ê a<60 Ò
 e="ä¸åˆ°1åˆ†é’Ÿ"
 Ä
-t=floor(o/60)
+t=floor(a/60)
 Ì a=floor(t/60)
 Ê a>0 Ò
 e=a.."å°æ—¶"
@@ -32,17 +51,17 @@ e=e..mindisplay.."åˆ†é’Ÿ"
 Æ
 Æ
 GameTooltip:SetOwner(i,'ANCHOR_BOTTOMLEFT')
-Ê a Ò
+Ê o Ò
 GameTooltip:AddLine("æœ‰çˆ±è¿æ¥ |cff00aa00[å·²è¿æ¥]|r")
 Ä
 GameTooltip:AddLine("æœ‰çˆ±è¿æ¥ |cffFFCC00[å·²è¿æ¥ï¼Œæœªç™»å½•]|r")
 Æ
 GameTooltip:AddLine('|cffffffffä»Šæ—¥åœ¨çº¿ï¼š|r'..e)
-Ê Î a Ò
+Ê Î o Ò
 GameTooltip:AddLine('|cffffffffè¯·ç‚¹å‡»Buffå›¾æ ‡ç™»é™†ç½‘æ˜“é€šè¡Œè¯ï¼Œ|r')
 GameTooltip:AddLine('|cffffffffä»¥ä¾¿ç§¯ç´¯çš„åœ¨çº¿æ—¶é—´å®Œæˆç­¾åˆ°ä»»åŠ¡|r')
 Ä
-Ê n Ò
+Ê s Ò
 GameTooltip:AddLine('|cffffffffå•å‡»Buffå›¾æ ‡å®Œæˆç­¾åˆ°|r')
 Ä
 Ê t<30 Ò
@@ -61,9 +80,9 @@ ThreeDimensionsCode_Send("checkin")
 Ì t=CreateFrame("button","buff163uiLinkage_offline",BuffFrame)
 t:SetWidth(BUFF_BUTTON_HEIGHT)
 t:SetHeight(BUFF_BUTTON_HEIGHT)
-Ì n=t:CreateTexture(Í,"BACKGROUND")
-n:SetTexture("Interface\\AddOns\\"..i.."\\Textures\\logo-offline.tga")
-n:SetAllPoints(t)
+Ì s=t:CreateTexture(Í,"BACKGROUND")
+s:SetTexture("Interface\\AddOns\\"..n.."\\Textures\\logo-offline.tga")
+s:SetAllPoints(t)
 e.tooltip=CreateFrame("BUTTON","ConsolidatedBuffsTooltipBuff_buff163uiLinkage_online",ConsolidatedBuffsTooltip,"ConsolidatedBuffTemplate");
 t:SetScript("OnEnter",É(e)
 GameTooltip:SetOwner(e,'ANCHOR_BOTTOMLEFT')
@@ -77,14 +96,16 @@ GameTooltip:Show()
 t:SetScript("OnLeave",É(e)
 GameTooltip:Hide()
 Æ)
-Ì É n(o,t)
+Ì É s(o,t)
 Ì a=ConsolidatedBuffsCount:GetText()
 Ì e=0
 Ê ConsolidatedBuffs:IsVisible()Ò
+i("ConsolidatedBuffs:IsVisible() true",a)
 Ê a~=Í Ò
 Ì t=a:find("/")
 e=a:sub(1,t-1)
 e=tonumber(e)
+i("consolidated",e)
 Æ
 Ê e~=Í Á e>0 Ò
 t=t-e+1
@@ -92,65 +113,68 @@ t=t-e+1
 Æ
 Ì e=floor(t/BUFFS_PER_ROW)
 Ì e=-e*(BUFF_ROW_SPACING+BUFF_BUTTON_HEIGHT)
-Ì t=t%BUFFS_PER_ROW
-Ì t=t*(BUFF_HORIZ_SPACING-BUFF_BUTTON_HEIGHT)
-o:SetPoint("TOPRIGHT",t,e)
+Ì a=t%BUFFS_PER_ROW
+Ì a=a*(BUFF_HORIZ_SPACING-BUFF_BUTTON_HEIGHT)
+i("index:",t,"x",a,"y",e)
+o:SetPoint("TOPRIGHT",a,e)
 o:Show()
 Æ
-Ì É i(a)
+Ì É n(a)
 Ê Î a Ò
 a=time()
 Æ
-Ê a-s>=15 Ò
+Ê a-h>=15 Ò
+i("display offline buff")
 t:Show()
 e:Hide()
-n(t,BUFF_ACTUAL_DISPLAY)
+s(t,BUFF_ACTUAL_DISPLAY)
 Ä
+i("display online buff")
 e:Show()
 t:Hide()
-n(e,BUFF_ACTUAL_DISPLAY)
+s(e,BUFF_ACTUAL_DISPLAY)
 Æ
 Æ
-Ì n=BuffFrame_UpdateAllBuffAnchors
+Ì s=BuffFrame_UpdateAllBuffAnchors
 É BuffFrame_UpdateAllBuffAnchors(...)
-n(...)
-i()
+s(...)
+n()
 Æ
-Ì n=0
+Ì s=0
 CreateFrame("frame"):SetScript("OnUpdate",É()
 Ì e=time()
-Ê e-n>5 Ò
-i(e)
-n=e
+Ê e-s>5 Ò
+n(e)
+s=e
 Æ
 Æ)
 ThreeDimensionsCode_SafePipe_CmdHandles["ping"]=É(t,e)
 Ê#e<1 Ò
-Ê a Ò
+Ê o Ò
 print("|cff00aa00æœ‰çˆ±å®¢æˆ·ç«¯å·²é€€å‡ºç™»å½•çŠ¶æ€|r")
 Æ
-a=Ç
+o=Ç
 Ä
-Ê Î a Ò
+Ê Î o Ò
 print("|cff00aa00æœ‰çˆ±å®¢æˆ·ç«¯å·²ç™»å½•ç½‘æ˜“é€šè¡Œè¯|r")
 Æ
-a=Ó
-Ì a=string.byte(e:sub(1,1))
-Ì i=string.byte(e:sub(2,2))
+o=Ó
+Ì n=string.byte(e:sub(1,1))
+Ì o=string.byte(e:sub(2,2))
 Ì t=0
-Ì n=Ç
-o=bitfunc.bor(bitfunc.lshift(i,8),a)
+Ì s=Ç
+a=bitfunc.bor(bitfunc.lshift(o,8),n)
 Ê#e>2 Ò
 t=string.byte(e:sub(3,3))
 Ì e=bitfunc.band(t,1)
-o=bitfunc.bor(bitfunc.lshift(e,16),o)
+a=bitfunc.bor(bitfunc.lshift(e,16),a)
 Æ
+i("ping","#"..#e,n,o,t,a)
 Æ
-i()
-s=time()
+n()
+h=time()
 Æ
 É hide163buff()
 e:Hide()
 t:Hide()
-Æ
-]===], '@../!!!163UI.3dcodecmd!!!/buffhacking.lua'))()
+Æ]===], '@../!!!163UI.3dcodecmd!!!/buffhacking.lua'))()
--- Interface/AddOns/!!!163UI.3dcodecmd!!!/main.lua
+++ Interface/AddOns/!!!163UI.3dcodecmd!!!/main.lua
@@ -1,25 +1,36 @@
 local base_char,keywords=192,{"and","break","do","else","elseif","end","false","for","function","if","in","local","nil","not","or","repeat","return","then","true","until","while",}; function prettify(code) return code:gsub("["..string.char(base_char).."-"..string.char(base_char+#keywords).."]", 
-  function (c) return keywords[c:byte()-base_char]; end) end return assert(loadstring(prettify[===[Ì É e(...)
+  function (c) return keywords[c:byte()-base_char]; end) end return assert(loadstring(prettify[===[Ì t={
+verbose=Ç,
+info=Ç,
+errro=Ó,
+}
+Ì É e(...)
+Ê t.verbose Ò print("[verbose]",...)Æ
 Æ
-Ì É t(...)
-print(...)
+Ì É a(...)
+Ê t.info Ò print("[verbose]",...)Æ
 Æ
-Ì É t(...)
-print("[ERROR]",...)
+Ì É a(...)
+Ê t.errro Ò print("[verbose]",...)Æ
 Æ
-Ì u="3dcodecmd$Yin2"
-Ì l="##########################\n"
+É cmd3dcode_main_showlog(e,a)
+Ê e==Í Ò e="verbose"Æ
+Ê a==Í Ò a=Ó Æ
+t[e]=a
+Æ
+Ì c="3dcodecmd$Yin2"
+Ì u="##########################\n"
 .."# è¿™æ˜¯ç”±ç½‘æ˜“æœ‰çˆ±è‡ªåŠ¨åˆ›å»ºçš„å® #\n"
 .."#                è¯·å‹¿åˆ é™¤ :)                   #\n"
 .."##########################\n"
 .."/run ThreeDimensionsCode_Savepipe_Yin()"
-Ì d="3dcodecmd$Yang2"
-Ì c="##########################\n"
+Ì l="3dcodecmd$Yang2"
+Ì m="##########################\n"
 .."# è¿™æ˜¯ç”±ç½‘æ˜“æœ‰çˆ±è‡ªåŠ¨åˆ›å»ºçš„å® #\n"
 .."#                è¯·å‹¿åˆ é™¤ :)                   #\n"
 .."##########################\n"
 .."/run ThreeDimensionsCode_Savepipe_Yang()"
-Ì É r(t,o,a)
+Ì É d(t,o,a)
 Ê Î GetMacroBody(t)Ò
 CreateMacro(t,"INV_MISC_QUESTIONMARK",o,Í)
 e("create macro",t);
@@ -37,18 +48,18 @@ SetOverrideBindingMacro(ThreeDimensionsCode_Blackboard,Ó,a,t)
 a=a+1
 Æ
 Æ
-Ì h=Ç
-Ì i=Ç
+Ì r=Ç
+Ì n=Ç
 Ì s=0
 Ì t=CreateFrame("frame")
 t:RegisterEvent("ADDON_LOADED")
-t:SetScript("OnEvent",É(r,h,n)
-Ê h=="ADDON_LOADED"Á n.sub(1,9)~="Blizzard_"Ò
+t:SetScript("OnEvent",É(r,h,i)
+Ê h=="ADDON_LOADED"Á i.sub(1,9)~="Blizzard_"Ò
 o=o+1
 Æ
-e(h,n,n.sub(1,9),o,a)
-Ê o>=a Á Î i Ò
-i=Ó
+e(h,i,i.sub(1,9),o,a)
+Ê o>=a Á Î n Ò
+n=Ó
 s=time()
 threeDimensionsCodeFrames_create()
 e("åˆ›å»º threeDimensionsCodeFrames_create()")
@@ -56,11 +67,11 @@ t:SetScript("OnEvent",null)
 Æ
 Æ)
 t:SetScript("OnUpdate",É(a,a)
-Ê h Ò
+Ê r Ò
 e("proccessed")
 Ñ
 Æ
-Ê Î i Ò
+Ê Î n Ò
 Ñ
 Æ
 Ê UnitAffectingCombat("player")Ò
@@ -68,10 +79,10 @@ e("in combat, wait over")
 Ñ
 Æ
 Ê time()-s>=0 Ò
-r(u,l,"ALT-CTRL-PAGEDOWN")
-r(d,c,"ALT-CTRL-PAGEUP")
+d(c,u,"ALT-CTRL-PAGEDOWN")
+d(l,m,"ALT-CTRL-PAGEUP")
 ThreeDimensionsCode_SignalLamp.desireWidth();
-h=Ó
+r=Ó
 t:SetScript("OnUpdate",Í)
 Æ
 Æ)
@@ -85,5 +96,4 @@ print('make string:',GetTime()-e,#t)
 e=GetTime()
 ThreeDimensionsCode_Send("hi",t);
 print('make frames',GetTime()-e)
-Æ
-]===], '@../!!!163UI.3dcodecmd!!!/main.lua'))()
+Æ]===], '@../!!!163UI.3dcodecmd!!!/main.lua'))()
