380e7b83172f78b16df2826ad7146b4587e87f5f
root <root@debian6-test.(none)>
2014-05-22 21:32:26 +0800

    build 3dcodecmd

--- Interface/AddOns/!!!163UI.3dcodecmd!!!/main.lua
+++ Interface/AddOns/!!!163UI.3dcodecmd!!!/main.lua
@@ -13,19 +13,19 @@ errro=Ó,
 Ì É a(...)
 Ê t.errro Ò print("[verbose]",...)Æ
 Æ
-É cmd3dcode_main_showlog(e,a)
-Ê e==Í Ò e="verbose"Æ
-Ê a==Í Ò a=Ó Æ
-t[e]=a
+É cmd3dcode_main_showlog(a,e)
+Ê a==Í Ò a="verbose"Æ
+Ê e==Í Ò e=Ó Æ
+t[a]=e
 Æ
-Ì c="3dcodecmd$Yin2"
-Ì u="##########################\n"
+Ì u="3dcodecmd$Yin2"
+Ì m="##########################\n"
 .."# è¿™æ˜¯ç”±ç½‘æ˜“æœ‰çˆ±è‡ªåŠ¨åˆ›å»ºçš„å® #\n"
 .."#                è¯·å‹¿åˆ é™¤ :)                   #\n"
 .."##########################\n"
 .."/run ThreeDimensionsCode_Savepipe_Yin()"
 Ì l="3dcodecmd$Yang2"
-Ì m="##########################\n"
+Ì f="##########################\n"
 .."# è¿™æ˜¯ç”±ç½‘æ˜“æœ‰çˆ±è‡ªåŠ¨åˆ›å»ºçš„å® #\n"
 .."#                è¯·å‹¿åˆ é™¤ :)                   #\n"
 .."##########################\n"
@@ -40,38 +40,50 @@ e("macro has already exists",t)
 SetOverrideBindingMacro(ThreeDimensionsCode_Blackboard,Ó,a,t)
 Æ
 Ì o=0
-Ì t=GetNumAddOns()
-Ì a=0
-È e=1,GetNumAddOns()Ã
-Ì o,o,o,t,o,o,o=GetAddOnInfo(e)
-Ê t Á Î IsAddOnLoadOnDemand(e)Ò
-a=a+1
-Æ
-Æ
 Ì r=Ç
-Ì n=Ç
-Ì s=0
-Ì t=CreateFrame("frame")
-t:RegisterEvent("ADDON_LOADED")
-t:SetScript("OnEvent",É(r,h,i)
-Ê h=="ADDON_LOADED"Á i.sub(1,9)~="Blizzard_"Ò
-o=o+1
+Ì t=GetNumAddOns()
+Ì i=0
+È e=1,GetNumAddOns()Ã
+Ì a,a,a,t,a,a,a=GetAddOnInfo(e)
+Ê t Á Î IsAddOnLoadOnDemand(e)Ò
+i=i+1
 Æ
-e(h,i,i.sub(1,9),o,a)
-Ê o>=a Á Î n Ò
-n=Ó
+Æ
+Ì n=Ç
+Ì a=Ç
+Ì s=0
+Ì c=time()
+Ì t=CreateFrame("frame")
+Ì É h()
+a=Ó
 s=time()
+Ê Î r Ò
 threeDimensionsCodeFrames_create()
+r=Ó
 e("åˆ›å»º threeDimensionsCodeFrames_create()")
+Æ
 t:SetScript("OnEvent",null)
 Æ
+t:RegisterEvent("ADDON_LOADED")
+t:SetScript("OnEvent",É(s,n,t)
+Ê n=="ADDON_LOADED"Á t:sub(1,9)~="Blizzard_"Ò
+o=o+1
+Æ
+e(n,t,t:sub(1,9),o,i)
+Ê o>=i Á Î a Ò
+h()
+Æ
 Æ)
-t:SetScript("OnUpdate",É(a,a)
-Ê r Ò
+t:SetScript("OnUpdate",É(o,o)
+Ê n Ò
 e("proccessed")
 Ñ
 Æ
-Ê Î n Ò
+Ê time()-c>=3 Á Î a Ò
+e("ç­‰å¾…æ‰€æœ‰æ’ä»¶åŠ è½½å®Œæˆè¶…æ—¶")
+h()
+Æ
+Ê Î a Ò
 Ñ
 Æ
 Ê UnitAffectingCombat("player")Ò
@@ -79,10 +91,10 @@ e("in combat, wait over")
 Ñ
 Æ
 Ê time()-s>=0 Ò
-d(c,u,"ALT-CTRL-PAGEDOWN")
-d(l,m,"ALT-CTRL-PAGEUP")
+d(u,m,"ALT-CTRL-PAGEDOWN")
+d(l,f,"ALT-CTRL-PAGEUP")
 ThreeDimensionsCode_SignalLamp.desireWidth();
-r=Ó
+n=Ó
 t:SetScript("OnUpdate",Í)
 Æ
 Æ)
