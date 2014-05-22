db369e4803e8ae011bfd6150529db0ac3384d53a
Aaron <aaron@geng.im>
2014-05-23 01:16:58 +0800

    Signed-off-by: Aaron <aaron@geng.im>

--- Interface/AddOns/BiggerTradeskillUI/cfgBiggerTradeSkillUI.lua
+++
@@ -1,9 +0,0 @@
-U1RegisterAddon("BiggerTradeSkillUI", {
-    title = "制造面板增强",
-    defaultEnable = 1,
-    tags = { TAG_INTERFACE, TAG_TRADING, TAG_GOOD},
-    icon = [[Interface\Icons\INV_Hammer_03]],
-	desc = "强化系统自带的制造技能面板，更高效的显示配方和材料。",
-	nopic = 1,
-
-})
