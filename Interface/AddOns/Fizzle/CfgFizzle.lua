12ec63d70f0d814a682477a5f4e695722d3d8bd1
Aaron <aaron@geng.im>
2014-05-24 00:43:49 +0800

    Signed-off-by: Aaron <aaron@geng.im>

--- Interface/AddOns/Fizzle/CfgFizzle.lua
+++ Interface/AddOns/Fizzle/CfgFizzle.lua
@@ -2,7 +2,6 @@
     title = "装备耐久",
     defaultEnable = 1,
     load = "LOGIN",
-
     tags = { TAG_INTERFACE,TAG_ITEM },
     icon = [[Interface\Icons\INV_Hammer_04]],
     desc = "边框品质着色、人物界面耐久度显示。",
