8e64da0cec2e3cb4fe8c925f4f3b3443a448e8ab
alee <alee@chou.im>
2014-07-23 23:51:13 +0800

    remove bagnon

--- Interface/AddOns/Bagnon_GuildBank/components/tabFrame.lua
+++
@@ -1,8 +0,0 @@
---[[
-	tabFrame.lua
-		A container object for tabs
---]]
-
-local Bagnon = LibStub('AceAddon-3.0'):GetAddon('Bagnon')
-local TabFrame = Bagnon:NewClass('GuildTabFrame', 'Frame', Bagnon.BagFrame)
-TabFrame.Button = Bagnon.GuildTab
--- Interface/AddOns/Bagnon_GuildBank/localization/pt.lua
+++
@@ -1,9 +0,0 @@
---[[
-	Bagnon Guild Bank Localization: Brasilian Portuguese
---]]
-
-local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-GuildBank', 'ptBR')
-if not L then return end
-
-L.Log1 = 'Registro de Transações'
-L.Log3 = 'Informação da Aba'
