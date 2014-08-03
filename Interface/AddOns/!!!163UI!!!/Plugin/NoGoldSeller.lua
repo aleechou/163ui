6255b678acd7bea1677c9c444a8b746ad015f45e
alee <alee@chou.im>
2014-08-03 23:37:04 +0800

    增加了一批用于管理关键词的命令(在游戏内打 /ngs 查看使用说明)

--- Interface/AddOns/!!!163UI!!!/Plugin/NoGoldSeller.lua
+++ Interface/AddOns/!!!163UI!!!/Plugin/NoGoldSeller.lua
@@ -130,15 +130,15 @@ if (GetLocale() == "zhTW") then
 	} 
   --(正體中文)在這裡修改關鍵字,以" "包括,以,分隔.
   NGSrep="|cff3399FFNGS:|r本次登錄到現在已經截獲|cff00ff00%d|r條廣告訊息."
-  NGSturnoff="|cff00ff00NoGoldSeller:出售金幣廣告信息屏蔽插件已經停用.輸入/NGS啟用.|r"
-  NGSturnon="|cff00ff00NoGoldSeller:出售金幣廣告信息屏蔽插件已經啟用.輸入/NGS停用.|r"
+  NGSturnoff="|cff00ff00NoGoldSeller:出售金幣廣告信息屏蔽插件已經停用.輸入/NGS on啟用.|r"
+  NGSturnon="|cff00ff00NoGoldSeller:出售金幣廣告信息屏蔽插件已經啟用.輸入/NGS off停用.|r"
   NGSrepFreq=20; --(正體中文)在這裡截獲報告頻率.幾條消息報告一次,就改此處為n.
 else if (GetLocale() == "zhCN") then
     NGSwords = {"平台交易","担保","纯手工","淘宝","游戏币","代打","代练","工作室","战点","手工金","手工G","皇冠店","一赔","点心","冲钻","店铺","皇冠","评级","小卡","大卡","套餐","手工带","1_85","塞纳","代刷","带刷","牛肉","1-85","元=","G=","消保","好评"} 
 	--(简体中文)在这里修改关键字,以" "包括,以,分隔.
     NGSrep="本次登录到现在已经截获|cff00ff00%d|r条广告信息。"
-    NGSturnoff="|cff00ff00NoGoldSeller:出售金币广告信息屏蔽插件已经停用。输入/NGS启用。|r"
-    NGSturnon="|cff00ff00NoGoldSeller:出售金币广告信息屏蔽插件已经启用。输入/NGS停用。|r"
+    NGSturnoff="|cff00ff00NoGoldSeller:出售金币广告信息屏蔽插件已经停用。输入/NGS on启用。|r"
+    NGSturnon="|cff00ff00NoGoldSeller:出售金币广告信息屏蔽插件已经启用。输入/NGS off停用。|r"
     NGSrepFreq=1000; --(简体中文)在这里修改报告频率。n条消息报告一次，就该此处为n。
   else
     DEFAULT_CHAT_FRAME:AddMessage("請注意!\nNoGoldSeller只能在zhTW,zhCN下運行,不支持您現在的遊戲語言版本!已經自行禁用.")
@@ -198,19 +198,125 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", IsGoldSeller)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_ADDON", IsGoldSeller) 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_TEXT_EMOTE", IsGoldSeller) 
 
+
+
+local function isInTable(t,v)
+	for i, vv in pairs(t) do
+		if vv==v then
+			return i
+		end
+	end
+	return false
+end
+local function removeFromTable(t,v)
+	for _, vv in pairs(t) do
+		if vv==v then
+			return true
+		end
+	end
+	return false
+end
+local function mergeTable(t1,t2)
+	local mergedVs = {}
+	for _, v in pairs(t2) do
+		if not isInTable(t1,v) then
+			tinsert(t1,v)
+			tinsert(mergedVs,v)
+		end
+	end
+	return mergedVs
+end
+local function diffTable(t1,t2)
+	local removed = {}
+	for _, v in pairs(t2) do
+		local i = isInTable(t1,v)
+		print(v,i)
+		if i then
+			table.remove(t1,i)
+			tinsert(removed,v)
+		end
+	end
+	return removed
+end
+local subcmdfuncs = {
+	["+keyword"] = function(...)
+		print("添加关键词：",unpack(mergeTable(NGSwords,{...})))
+	end ,
+	["-keyword"] = function(...)
+		print("移除关键词：",unpack(diffTable(NGSwords,{...})))
+	end ,
+	["+symbols"] = function(...)
+		print("添加过滤字符：",unpack(mergeTable(NGSSymbols,{...})))
+	end ,
+	["-symbols"] = function(...)
+		print("移除过滤字符：",unpack(diffTable(NGSSymbols,{...})))
+	end ,
+	["-l"] = function()
+		print("NoGoldSeller 使用中的关键词：")
+		print(unpack(NGSwords))
+		print("NoGoldSeller 使用中的过滤字符：")
+		print(unpack(NGSSymbols))
+	end ,
+	["on"] = function(...)
+		DEFAULT_CHAT_FRAME:AddMessage(NGSturnon)
+		NGSenable=1
+	end ,
+	["off"] = function(...)
+		DEFAULT_CHAT_FRAME:AddMessage(NGSturnoff)
+		NGSenable=1
+	end ,
+	["?"] = function(...)
+		print("[启用NoGoldSeller聊天框过滤]")
+		print(" /ngs on")
+		print(" ")
+
+		print("[关闭NoGoldSeller聊天框过滤]")
+		print(" /ngs off")
+		print(" ")
+
+		print("[增加关键词]")
+		print(" /ngs +k 关键词...")
+		print(" 或 /ngs + 关键词...")
+		print(" 例如： /ngs + 淘宝 代练 90级")
+		print(" ")
+		print("[移除关键词]")
+		print(" /ngs -k 关键词...")
+		print(" 或 /ngs - 关键词...")
+		print(" 例如： /ngs - 淘宝 代练 90级")
+		print(" ")
+
+		print("[增加过滤字符]")
+		print(" /ngs +s 关键词...")
+		print(" 例如： /ngs + ① ② ③")
+		print(" ")
+		print("[移除过滤字符]")
+		print(" /ngs -s 关键词...")
+		print(" 例如： /ngs - ① ② ③")
+		print(" ")
+
+		print("[查看已添加的关键词和过滤字符]")
+		print(" /ngs -l")
+		print(" ")
+	end ,
+}
+subcmdfuncs["+"] = subcmdfuncs["+keyword"]
+subcmdfuncs["+k"] = subcmdfuncs["+keyword"]
+subcmdfuncs["-"] = subcmdfuncs["-keyword"]
+subcmdfuncs["-k"] = subcmdfuncs["-keyword"]
+subcmdfuncs["+s"] = subcmdfuncs["+symbols"]
+subcmdfuncs["-s"] = subcmdfuncs["-symbols"]
+
 SLASH_NGS1 = "/nogoldseller";
 SLASH_NGS2 = "/NGS";
-SlashCmdList["NGS"] = function(cmd)
-  if (NGSenable==2) then
-	DEFAULT_CHAT_FRAME:AddMessage("請注意!\nNoGoldSeller只能在zhTW,zhCN下運行,不支持您現在的遊戲語言版本!已經自行禁用.")
-	DEFAULT_CHAT_FRAME:AddMessage("WARNING!\nNoGoldSeller: This addon ONLY fits for Traditional Chinese (zhTW) & Simplified Chinese (zhCN) realms. Unsupport your game client. It has automatically disabled now.")
-  end
-  if (NGSenable==1) then
-	DEFAULT_CHAT_FRAME:AddMessage(NGSturnoff)
-	NGSenable=0;
-  else
-    DEFAULT_CHAT_FRAME:AddMessage(NGSturnon)
-    NGSenable=1;
-  end
+SlashCmdList["NGS"] = function(sargs,editor)
+	local args = {}
+	for _,v in pairs({strsplit(" ",sargs)}) do
+		if #v>0 then
+			tinsert(args,v)
+		end
+	end
+	local subcmd = args[1]
+	local func = (subcmd and subcmdfuncs[subcmd]) and subcmdfuncs[subcmd] or subcmdfuncs["?"]
+	func( select(2,unpack(args)) )
 end
   
