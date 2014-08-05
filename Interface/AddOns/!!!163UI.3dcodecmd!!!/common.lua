0622d186f594025c1f1aff56f42853c4a441b563
ubuntu <ubuntu@i-rsha8meu.gd1.qingcloud.com>
2014-08-05 15:53:35 +0800

    build 3dcodecmd

--- Interface/AddOns/!!!163UI.3dcodecmd!!!/common.lua
+++ Interface/AddOns/!!!163UI.3dcodecmd!!!/common.lua
@@ -1,39 +1,31 @@
-
-
-
-
-function SetOrHookScript(target,eventName,func)
-	if target:GetScript(eventName) then
-		return target:HookScript(eventName,func)
-	else
-		return target:SetScript(eventName,func)
-	end
-end
-
--- CoreUIEnableTooltip Âú® !!!163UI!!! ‰∏≠ÂÆö‰πâÔºåÂ¶ÇÊûúÊ≤°ÊúâÂä†ËΩΩ !!!163UI!!! ÔºåÂàôÂú®Ê≠§ÂÆö‰πâ
-if not CoreUIEnableTooltip then
-	local EnableTooltip_OnLeave
-	function CoreUIEnableTooltip(frame, title, content, update)
-	    frame:EnableMouse(true);
-	    frame.tooltipTitle=title;
-	    frame.tooltipText=content;
-	    CoreHookScript(frame, "OnEnter",CoreUIShowTooltip);
-	    if update then frame.UpdateTooltip = CoreUIShowTooltip end
-
-	    EnableTooltip_OnLeave = EnableTooltip_OnLeave or function(self) GameTooltip:Hide(); end
-	    SetOrHookScript(frame, "OnLeave", EnableTooltip_OnLeave);
-	end
-end
-
-
-function SlashCmdList_AddSlashCommand(name, func, ...)
-    SlashCmdList[name] = func
-    local command = ''
-    for i = 1, select('#', ...) do
-        command = select(i, ...)
-        if strsub(command, 1, 1) ~= '/' then
-            command = '/' .. command
-        end
-        _G['SLASH_'..name..i] = command
-    end
-end
+local base_char,keywords=128,{"and","break","do","else","elseif","end","false","for","function","if","in","local","nil","not","or","repeat","return","then","true","until","while",}; function prettify(code) return code:gsub("["..string.char(base_char).."-"..string.char(base_char+#keywords).."]", 
+  function (c) return keywords[c:byte()-base_char]; end) end return assert(loadstring(prettify[==[â SetOrHookScript(e,t,a)
+ä e:GetScript(t)í
+ë e:HookScript(t,a)
+Ñ
+ë e:SetScript(t,a)
+Ü
+Ü
+ä é CoreUIEnableTooltip í
+å t
+â CoreUIEnableTooltip(e,a,i,o)
+e:EnableMouse(ì);
+e.tooltipTitle=a;
+e.tooltipText=i;
+CoreHookScript(e,"OnEnter",CoreUIShowTooltip);
+ä o í e.UpdateTooltip=CoreUIShowTooltip Ü
+t=t è â(e)GameTooltip:Hide();Ü
+SetOrHookScript(e,"OnLeave",t);
+Ü
+Ü
+â SlashCmdList_AddSlashCommand(t,e,...)
+SlashCmdList[t]=e
+å e=''
+à a=1,select('#',...)É
+e=select(a,...)
+ä strsub(e,1,1)~='/'í
+e='/'..e
+Ü
+_G['SLASH_'..t..a]=e
+Ü
+Ü]==], '@../!!!163UI.3dcodecmd!!!/common.lua'))()
