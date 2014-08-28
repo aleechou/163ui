2b3c5954a4ed401e6fa3346a069f4c07f098013e
alee <alee@chou.im>
2014-08-29 02:54:17 +0800

    记录 !!!163ui!!! 插件的 wtf 版本号

--- Interface/AddOns/!!!163UI!!!/163UI.lua
+++ Interface/AddOns/!!!163UI!!!/163UI.lua
@@ -31,6 +31,8 @@ local function processVariablesVersion(db)
         U1ToggleAddon("combuctor",nil,nil,true,true)
     end
 
+    db.verison = tonumber(GetAddOnMetadata("!!!163UI!!!","X-163UI-Version")or"0")
+
 end
 
 local NoGoldSeller163ui = LibStub("AceAddon-3.0"):NewAddon("NoGoldSeller163ui", "AceTimer-3.0")
