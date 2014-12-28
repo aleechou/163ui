354933e40eddeaaca9aac59d00e04dfcd0a1dfac
alee <alee@chou.im>
2014-12-29 00:06:53 +0800

    fix: 修复 tiptac 设置界面里的所有勾选按钮失效的问题

--- Interface/AddOns/TipTacOptions/Libs/AzOptionsFactory.lua
+++ Interface/AddOns/TipTacOptions/Libs/AzOptionsFactory.lua
@@ -141,7 +141,7 @@ local function CheckButton_OnLeave(self)
 end
 
 local function CheckButton_OnClick(self)
-	self.factory:ChangeSettingFunc(self.option.var,self:GetChecked() == 1);
+	self.factory:ChangeSettingFunc(self.option.var,self:GetChecked() == true);
 end
 
 -- New CheckButton
